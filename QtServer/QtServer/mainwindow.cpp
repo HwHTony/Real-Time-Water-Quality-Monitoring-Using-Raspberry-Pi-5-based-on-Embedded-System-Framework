3ac785d061e9f8a0f826e0b3ee004dceeafc1ba6
mr_zhou <mr_zhou@163.com>
2025-03-26 11:59:01 +0800

    [Qt Init QTcpServer]

--- QtServer/QtServer/mainwindow.cpp
+++ QtServer/QtServer/mainwindow.cpp
@@ -1,11 +1,62 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+#include <QMessageBox>
+#include <QHostAddress>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , tcpServer(new QTcpServer(this))
+    , clientSocket(nullptr)
 {
     ui->setupUi(this);
+    setupServer();
+}
+
+void MainWindow::setupServer()
+{
+    // 启动服务器并监听端口
+    if (!tcpServer->listen(QHostAddress::Any, 12345)) {
+        QMessageBox::critical(this, "Server Error",
+                             tr("Unable to start server: %1")
+                             .arg(tcpServer->errorString()));
+        return;
+    }
+
+    // 连接新连接信号
+    connect(tcpServer, &QTcpServer::newConnection,
+            this, &MainWindow::handleNewConnection);
+
+    ui->statusLabel->setText("🟢 Server Running (Port: 12345)");
+}
+
+void MainWindow::handleNewConnection()
+{
+    // 拒绝已有连接的新请求
+    if (clientSocket) {
+        QTcpSocket *newSocket = tcpServer->nextPendingConnection();
+        newSocket->write("Another client is already connected!");
+        newSocket->disconnectFromHost();
+        return;
+    }
+
+    // 获取客户端套接字
+    clientSocket = tcpServer->nextPendingConnection();
+
+    connect(clientSocket, &QTcpSocket::disconnected,
+            this, &MainWindow::clientDisconnected);
+
+    ui->statusLabel->setText("🔵 Client Connected: " +
+                            clientSocket->peerAddress().toString());
+}
+
+void MainWindow::clientDisconnected()
+{
+    clientSocket->deleteLater();
+    clientSocket = nullptr;
+    dataBuffer.clear();
+    ui->statusLabel->setText("🟠 Client Disconnected");
 }
 
 MainWindow::~MainWindow()
--- QtServer/QtServer/mainwindow.h
+++ QtServer/QtServer/mainwindow.h
@@ -1,7 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +22,20 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
+public slots:
+    void handleNewConnection();
+    void clientDisconnected();
+
+private:
+    void setupServer();
+
 private:
     Ui::MainWindow *ui;
+
+    QTcpServer *tcpServer;
+    QTcpSocket *clientSocket;
+    QByteArray dataBuffer; // 用于累积接收的数据
 };
 
 #endif // MAINWINDOW_H
