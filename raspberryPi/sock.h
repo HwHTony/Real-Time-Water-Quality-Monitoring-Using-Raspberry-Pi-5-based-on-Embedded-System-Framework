877765d4cc86c6daf2670faa64ca3a9e190e5c46
RYC2960734 <chenruiyang1012@outlook.com>
2025-04-08 23:29:16 +0800

    add sock source file, ini tcp socket

---
+++ raspberryPi/sock.h
@@ -0,0 +1,13 @@
+#ifndef SOCK_H
+#define SOCK_H
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "com.h"
+
+#define PORT 8888
+#define BUFFER_SIZE 1024
+
+#endif
