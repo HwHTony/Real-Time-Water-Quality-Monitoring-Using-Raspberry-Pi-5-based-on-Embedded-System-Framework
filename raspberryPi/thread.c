a521fc08d63b9efeec070d0e4da6bbcb8353c116
RYC2960734 <chenruiyang1012@outlook.com>
2025-04-11 17:33:11 +0800

    add thread source file and head

---
+++ raspberryPi/thread.c
@@ -0,0 +1,17 @@
+#include "thread.h"
+
+int pthread_init(pthread_t *id, void *(*fun)(void* arg))
+{
+    if (pthread_create(id, NULL, fun, NULL))
+    {
+        perror("create thread failed\n");
+        return -1;
+    }
+
+    // 立即分离线程
+    if (pthread_detach(*id) != 0) {
+        perror("pthread_detach 失败");
+        return -1;
+    }
+    return 0;
+}
---
+++ raspberryPi/thread.h
@@ -0,0 +1,10 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int pthread_init(pthread_t *id, void *(*fun)(void* arg));
+
+#endif 
