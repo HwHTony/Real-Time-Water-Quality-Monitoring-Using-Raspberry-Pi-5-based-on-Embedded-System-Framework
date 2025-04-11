4bee48e9271f78d9d3c4640f98a3adc463fa0b17
RYC2960734 <chenruiyang1012@outlook.com>
2025-04-11 17:33:51 +0800

    Update thread.c

--- raspberryPi/thread.c
+++ raspberryPi/thread.c
@@ -8,7 +8,7 @@ int pthread_init(pthread_t *id, void *(*fun)(void* arg))
         return -1;
     }
 
-    // 立即分离线程
+    // thread detach when created
     if (pthread_detach(*id) != 0) {
         perror("pthread_detach 失败");
         return -1;
