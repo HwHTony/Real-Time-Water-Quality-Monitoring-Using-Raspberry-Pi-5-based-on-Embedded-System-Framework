93f58e6687212229a4dcda3e2f321ab1170fa880
RYC2960734 <chenruiyang1012@outlook.com>
2025-07-24 22:08:43 +0100

    Update app.h

--- Raspberrry Pi/src/app/app.h
+++ Raspberrry Pi/src/app/app.h
@@ -9,7 +9,7 @@
 #include "../data_collection/data_collector.h"
 #include "../info_updating/info_updater.h"
 #include "../networking/sock.h"
-#include <signal.h> // 添加 <signal.h> 头文件
+#include <signal.h> // add <signal.h> header file
 
 class App {
 private:
@@ -19,13 +19,13 @@ private:
     std::vector<InfoUpdater*> updaters;
     std::vector<int> timer_fds;
 
-    // 信号处理函数
+    // signal processing function
     static void sigint_handler(int signum, siginfo_t *info, void *context);
 
-    // 创建定时器fd
+    // Create timer fd
     int create_timer_fd(int interval_ms);
 
-    // 读取定时器事件
+    // Read timer event
     void read_timer_fd(int fd);
 
 public:
