202fcb0fbed6beacae86d8f7fd9314003f95346d
linsanxixi <2961262W@student.gla.ac.uk>
2025-07-25 00:51:34 +0100

    Update tft_info_updater.cpp

--- Raspberrry Pi/src/info_updating/tft_info_updater.cpp
+++ Raspberrry Pi/src/info_updating/tft_info_updater.cpp
@@ -6,10 +6,10 @@
 
 void TFTInfoUpdater::update() {
     memset(turb, ' ', BUFFER_SIZE);
-    std::swprintf(turb, sizeof(turb) / sizeof(wchar_t), L"浊度: %.2f", WaterQuality::getInstance().getTurbidity());
+    std::swprintf(turb, sizeof(turb) / sizeof(wchar_t), L"Turbidity: %.2f", WaterQuality::getInstance().getTurbidity());
     tft.drawString(5, 20, turb,  0xFFFF);
 
-    std::swprintf(turb, sizeof(turb) / sizeof(wchar_t), L"温度: %.2f℃", WaterQuality::getInstance().getDS18B20());
+    std::swprintf(turb, sizeof(turb) / sizeof(wchar_t), L"Temperature: %.2f℃", WaterQuality::getInstance().getDS18B20());
     tft.drawString(5, 50, turb,  0xFFFF);
 
     std::swprintf(turb, sizeof(turb) / sizeof(wchar_t), L"pH: %.2f", WaterQuality::getInstance().getpH());
